d8cc4dbbefae625c39ac7f5128c761b736f7542e
Serge Paliichuk <47915260+Serge1212@users.noreply.github.com>
2021-05-29 16:16:02 +0300

    test change

--- DigichList.Backend/Startup.cs
+++ DigichList.Backend/Startup.cs
@@ -17,8 +17,7 @@ namespace DigichList.Backend
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers().AddNewtonsoftJson();
@@ -30,7 +29,6 @@ namespace DigichList.Backend
             services.AddDbContext<DigichListContext>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())

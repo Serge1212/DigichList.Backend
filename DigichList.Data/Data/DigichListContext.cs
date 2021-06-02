805787d7649945c141a5ecbf9a18414c696a37ab
Serge <sanichace@gmail.com>
2021-06-03 01:25:22 +0300

    changed db connection credentials

--- DigichList.Data/Data/DigichListContext.cs
+++ DigichList.Data/Data/DigichListContext.cs
@@ -9,7 +9,9 @@ namespace DigichList.Infrastructure.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DigichListDb;Trusted_Connection=True;");
-            optionsBuilder.UseNpgsql("Server=127.0.0.1; port=5432; user id=postgres; password=postgresidk; database=DigichListDb; pooling=true");
+            optionsBuilder.UseNpgsql("Server=ec2-34-247-118-233.eu-west-1.compute.amazonaws.com;" +
+                " port=5432; user id=qvjmmqjpewzsne; password=1e8c63da9337fbc7bf354e9154ac130881d7d4b8b9aa84c6311fdcadc6f3f422;" +
+                " database=dcu1kak5dscd9a; pooling=true");
         }
 
         public DbSet<Admin> Admins { get; set; }

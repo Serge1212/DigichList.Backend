31014c904bcfccb0ca89ab09aea11ee2e4e02ec5
Serge <sanichace@gmail.com>
2021-06-19 02:40:47 +0300

    changed api key for telegram bot

--- DigichList.TelegramNotifications/BotConfig/BotClient.cs
+++ DigichList.TelegramNotifications/BotConfig/BotClient.cs
@@ -4,6 +4,6 @@ namespace DigichList.TelegramNotifications.BotConfig
 {
     internal static class BotClient
     {
-        internal static readonly TelegramBotClient Bot = new TelegramBotClient("1709499574:AAHfbShmNa_nRF_s5cNGD9D8RMfzwMdPjMs");
+        internal static readonly TelegramBotClient Bot = new TelegramBotClient("1709499574:AAGlBEt4Hav2udT55wqXtoP12z7GwhRNPkI");
     }
 }
